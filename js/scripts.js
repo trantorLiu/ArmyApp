3656aec6c54ade5e66f1a1564c57e01589661a37
Bingdow <bingdow@gmail.com>
2014-01-25 21:53:11 +0800

    info navigation problem fixed.

--- js/scripts.js
+++ js/scripts.js
@@ -15,21 +15,38 @@ $(document).ready(function(){
 			 autoControls: false
 		});
 
+		var slideToInfo = function(node){
+			var transform_N = -270;
+			//alert($(node).attr('data-slide-index'));
+			var translate3d_N = 'translate3d('+(parseInt($(node).attr('data-slide-index'))+1)*parseInt(transform_N)+'px, 0px, 0px)';
+			$('ul.bxslider.appslider').css('transition','0.5s');
+			$('ul.bxslider.appslider').css('transform', translate3d_N);
+			return false;
+		}
 
+		$('div.android-image a.bx-pager-link').click(function(e){
+			e.preventDefault();
+			e.stopPropagation();
+			slideToInfo(this);
+		});
 		$('.first-box').hover(function(){
-            $('ul.bxslider.appslider').css('transition','0.5s');
-            $('ul.bxslider.appslider').css('transform','translate3d(-270px, 0px, 0px)');
+			slideToInfo(this);
+            //$('ul.bxslider.appslider').css('transition','0.5s');
+            //$('ul.bxslider.appslider').css('transform','translate3d(-270px, 0px, 0px)');
 			return false;
 		});
 		$('.second-box').hover(function(){
-            $('ul.bxslider.appslider').css('transition','0.5s');
-            $('ul.bxslider.appslider').css('transform','translate3d(-540px, 0px, 0px)');
+			slideToInfo(this);
+            //$('ul.bxslider.appslider').css('transition','0.5s');
+            //$('ul.bxslider.appslider').css('transform','translate3d(-540px, 0px, 0px)');
 			return false;
 
 		});
 		$('.third-box').hover(function(){
-            $('ul.bxslider.appslider').css('transition','0.5s');
-            $('ul.bxslider.appslider').css('transform','translate3d(-810px, 0px, 0px)');
+			slideToInfo(this);
+            //$('ul.bxslider.appslider').css('transition','0.5s');
+            //$('ul.bxslider.appslider').css('transform','translate3d(-810px, 0px, 0px)');
 			return false;
 		});
+
 });
