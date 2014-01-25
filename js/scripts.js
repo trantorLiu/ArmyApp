1239829a2b5f0f2bc42c15139ec5e18dda4c5adf
Bingdow <bingdow@gmail.com>
2014-01-25 18:08:50 +0800

    smooth slide added.

--- js/scripts.js
+++ js/scripts.js
@@ -17,16 +17,19 @@ $(document).ready(function(){
 
 
 		$('.first-box').hover(function(){
+            $('ul.bxslider.appslider').css('transition','0.5s');
             $('ul.bxslider.appslider').css('transform','translate3d(-270px, 0px, 0px)');
 			return false;
 		});
 		$('.second-box').hover(function(){
+            $('ul.bxslider.appslider').css('transition','0.5s');
             $('ul.bxslider.appslider').css('transform','translate3d(-540px, 0px, 0px)');
 			return false;
 
 		});
 		$('.third-box').hover(function(){
-            $('ul.bxslider.appslider').css('transform','translate3d(0px, 0px, 0px)');
+            $('ul.bxslider.appslider').css('transition','0.5s');
+            $('ul.bxslider.appslider').css('transform','translate3d(-810px, 0px, 0px)');
 			return false;
 		});
 });
