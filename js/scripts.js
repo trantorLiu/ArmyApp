44a0fdcc8cc252419558ea4dd9d64fa5adade501
Bingdow <bingdow@gmail.com>
2014-01-25 23:45:25 +0800

    revert back

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,19 @@ www.dannypostma.com
 I think that's a shit plugin. My grandma is better than him.
 */
 "use strict";
-var slideToInfo = function(node){
+$(document).ready(function(){
+		$('.testimonialsslider').bxSlider({
+			 auto: true,
+			 autoControls: false
+		});
+		
+		
+		var sliderTestimonial = $('.appslider').bxSlider({
+			 auto: false,
+			 autoControls: false
+		});
+
+		var slideToInfo = function(node){
 			var transform_N = -270;
 			var translate3d_N = 'translate3d('+((parseInt($(node).attr('data-slide-index'))+1)*parseInt(transform_N)).toString()+'px, 0px, 0px)';
 			$('ul.bxslider.appslider').css('transition','0.5s');
@@ -15,26 +27,18 @@ var slideToInfo = function(node){
 			$('div.android-image a.bx-pager-link').eq(parseInt($(this).attr('data-slide-index'))).css('background','none repeat scroll 0 0 #000000');
 			return false;
 		}
-$(document).ready(function(){
-		"use strict";
-		$('.testimonialsslider').bxSlider({
-			 auto: true,
-			 autoControls: false
-		});
-		
-		
-		$('.appslider').bxSlider({
-			 auto: false,
-			 autoControls: false
-		});
 
 		$('div.android-image a.bx-pager-link').click(function(e){
 			e.preventDefault();
 			e.stopPropagation();
 			slideToInfo(this);
+			$('div.android-image a.bx-pager-link').css('background','none repeat scroll 0 0 #666666');
+			$(this).css('background','none repeat scroll 0 0 #000000');
 		});
 		$('div.info-box').hover(function(){
 			slideToInfo(this);
+			$('div.android-image a.bx-pager-link').css('background','none repeat scroll 0 0 #666666');
+			$('div.android-image a.bx-pager-link').eq(parseInt($(this).attr('data-slide-index'))).css('background','none repeat scroll 0 0 #000000');
 			return false;
 		});
 });
