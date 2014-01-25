7d8ba8beddf091b933b52ba0e38004ef2fc17783
Bingdow <bingdow@gmail.com>
2014-01-25 23:36:35 +0800

    fight with navigation problem.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,16 @@ www.dannypostma.com
 /*
 I think that's a shit plugin. My grandma is better than him.
 */
+"use strict";
+var slideToInfo = function(node){
+			var transform_N = -270;
+			var translate3d_N = 'translate3d('+((parseInt($(node).attr('data-slide-index'))+1)*parseInt(transform_N)).toString()+'px, 0px, 0px)';
+			$('ul.bxslider.appslider').css('transition','0.5s');
+			$('ul.bxslider.appslider').css('transform', translate3d_N);
+			$('div.android-image a.bx-pager-link').css('background','none repeat scroll 0 0 #666666');
+			$('div.android-image a.bx-pager-link').eq(parseInt($(this).attr('data-slide-index'))).css('background','none repeat scroll 0 0 #000000');
+			return false;
+		}
 $(document).ready(function(){
 		"use strict";
 		$('.testimonialsslider').bxSlider({
@@ -17,16 +27,6 @@ $(document).ready(function(){
 			 auto: false,
 			 autoControls: false
 		});
-
-		var slideToInfo = function(node){
-			var transform_N = -270;
-			var translate3d_N = 'translate3d('+((parseInt($(node).attr('data-slide-index'))+1)*parseInt(transform_N)).toString()+'px, 0px, 0px)';
-			$('ul.bxslider.appslider').css('transition','0.5s');
-			$('ul.bxslider.appslider').css('transform', translate3d_N);
-			$('div.android-image a.bx-pager-link').css('background','none repeat scroll 0 0 #666666');
-			$('div.android-image a.bx-pager-link').eq(parseInt($(this).attr('data-slide-index'))).css('background','none repeat scroll 0 0 #000000');
-			return false;
-		}
 
 		$('div.android-image a.bx-pager-link').click(function(e){
 			e.preventDefault();
