349fcbd55ed2702a4f27f626b2bfccf62ec59af4
Bingdow <bingdow@gmail.com>
2014-01-25 22:40:47 +0800

    Fucking phone navigation style change work done. Fx and GC works fine.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-/* Author:
+/* bxSlider Author:
 DANNY POSTMA
 www.dannypostma.com
+*/
+/*
+I think that's a shit plugin. My grandma is better than him.
 */
 $(document).ready(function(){
 		"use strict";
@@ -17,7 +20,6 @@ $(document).ready(function(){
 
 		var slideToInfo = function(node){
 			var transform_N = -270;
-			//alert($(node).attr('data-slide-index'));
 			var translate3d_N = 'translate3d('+((parseInt($(node).attr('data-slide-index'))+1)*parseInt(transform_N)).toString()+'px, 0px, 0px)';
 			$('ul.bxslider.appslider').css('transition','0.5s');
 			$('ul.bxslider.appslider').css('transform', translate3d_N);
@@ -27,26 +29,15 @@ $(document).ready(function(){
 		$('div.android-image a.bx-pager-link').click(function(e){
 			e.preventDefault();
 			e.stopPropagation();
+			$('div.android-image a.bx-pager-link').css('background','none repeat scroll 0 0 #666666');
+			$(this).css('background','none repeat scroll 0 0 #000000');
 			slideToInfo(this);
 		});
-		$('.first-box').hover(function(){
-			//slideToInfo(this);
-            //$('ul.bxslider.appslider').css('transition','0.5s');
-            //$('ul.bxslider.appslider').css('transform','translate3d(-270px, 0px, 0px)');
-			return false;
-		});
-		$('.second-box').hover(function(){
+		$('div.info-box').hover(function(){
+			var phone_button;
 			slideToInfo(this);
-            //$('ul.bxslider.appslider').css('transition','0.5s');
-            //$('ul.bxslider.appslider').css('transform','translate3d(-540px, 0px, 0px)');
-			return false;
-
-		});
-		$('.third-box').hover(function(){
-			slideToInfo(this);
-            //$('ul.bxslider.appslider').css('transition','0.5s');
-            //$('ul.bxslider.appslider').css('transform','translate3d(-810px, 0px, 0px)');
+			$('div.android-image a.bx-pager-link').css('background','none repeat scroll 0 0 #666666');
+			$('div.android-image a.bx-pager-link').eq(parseInt($(this).attr('data-slide-index'))).css('background','none repeat scroll 0 0 #000000');
 			return false;
 		});
-
 });
